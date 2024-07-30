c4effcc44f5b45d1b41d3941f424b140777f7cbc
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2024-07-30 16:23:14 -0500

    Agregue una linea de comentario

--- src/main.py
+++ src/main.py
@@ -41,5 +41,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+#Ejemplo de comando
 #python main.py -i "D:\Code\next_gen\packages\agrilac_nextgen_packages\config" -o "D:\Code\next_gen\packages\agrilac_nextgen_packages"
